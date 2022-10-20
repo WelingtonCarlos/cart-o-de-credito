e761e144ecce5b3a262bf2f753848ce6ee615f19
Welington <welington.carlos@mrpay.com.br>
2022-10-20 17:16:05 -0300

    atualizando projeto

--- src/main.js
+++ src/main.js
@@ -1,15 +1,18 @@
 import "./css/index.css"
+import IMask from "imask"
 
-// mudando bandeira do cartão -------------------
-const ccBgColor01 = document.querySelector(".cc-bg svg > g g:nth-child(1) path")
-const ccBgColor02 = document.querySelector(".cc-bg svg > g g:nth-child(2) path")
+// mudando bandeira do cartão ---------------------------------------------------------
+const ccBgColor01 = document.querySelector("#first-bg")
+const ccBgColor02 = document.querySelector("#second-bg")
 
-const ccLogo = document.querySelector(".cc-logo span:nth-child(2) > img")
+const ccLogo = document.querySelector("#cc-logo")
 
 function setCardType(type) {
   const colors = {
     visa: ["#436D99", "#2d57f2"],
     mastercard: ["#DF6F29", "#C69347"],
+    amex: ["#bfac66", "#bfac66"],
+    diners: ["#6e4464", "#6e4464"],
     default: ["black", "gray"],
   }
 
@@ -18,6 +21,93 @@ function setCardType(type) {
   ccLogo.setAttribute("src", `cc-${type}.svg`)
 }
 
+// Mask Card Number
+const cardNumber = document.querySelector("#card-number")
+const cardNumberPattern = {
+  mask: [
+    {
+      mask: "0000 0000 0000 0000",
+      regex: /^4\d{0,15}/,
+      cardtype: "visa",
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      regex: /(^5[1-5]\d{0,2}|^22[2-9]\d|^2[3-7]\d{0,2})\d{0,12}/,
+      cardtype: "mastercard",
+    },
+    {
+      mask: "0000 000000 00000",
+      regex: /^3[47]\d{0,13}/,
+      cardtype: "american express",
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      regex: /^(?:6011|65\d{0,2}|64[4-9]\d?)\d{0,12}/,
+      cardtype: "discover",
+    },
+    {
+      mask: "0000 000000 0000",
+      regex: /^3(?:0([0-5]|9)|[689]\d?)\d{0,11}/,
+      cardtype: "diners",
+    },
+    {
+      mask: "0000 000000 00000",
+      regex: /^(?:2131|1800)\d{0,11}/,
+      cardtype: "jcb15",
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      regex: /^(?:35\d{0,2})\d{0,12}/,
+      cardtype: "jcb",
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      regex: /^62\d{0,14}/,
+      cardtype: "unionpay",
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      cardtype: "default",
+    },
+  ],
+  dispatch: function (appended, dynamicMasked) {
+    const number = (dynamicMasked.value + appended).replace(/\D/g, "")
+    const foundMask = dynamicMasked.compiledMasks.find(function (item) {
+      return number.match(item.regex)
+    })
+
+    console.log(foundMask)
+    return foundMask
+  },
+}
+const cardNumberMasked = IMask(cardNumber, cardNumberPattern)
+
+// Mask CVC
+const securityCode = document.querySelector("#security-code")
+const securityCodePattern = {
+  mask: "0000",
+}
+const securityCodeMasked = IMask(securityCode, securityCodePattern)
+
+// Mask Validity
+const validityDate = document.querySelector("#expiration-date")
+const validityDatePattern = {
+  mask: "MM{/}YY",
+  blocks: {
+    YY: {
+      mask: IMask.MaskedRange,
+      from: String(new Date().getFullYear()).slice(2),
+      to: String(new Date().getFullYear() + 5).slice(2),
+    },
+    MM: {
+      mask: IMask.MaskedRange,
+      from: 1,
+      to: 12,
+    },
+  },
+}
+const validityDateMasked = IMask(validityDate, validityDatePattern)
+
 globalThis.setCardType = setCardType
 
-// ----------------------------------------------------
+// -------------------------------------------------------------------------------------
